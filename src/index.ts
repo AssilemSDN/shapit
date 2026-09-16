0056029e534d877f53904be708cb93c37ec4863c
Mélissa <pufferfish.bloop@proton.me>
2026-09-16 14:57:59 +0200

    chore: add types to api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 export { loadConfig } from './config/load-config.js'
 export { audit } from './core/audit.js'
 
+export { AppError } from './errors/AppError.js'
+export { ErrorCodes } from './errors/error-codes.js'
+
+export type { ErrorCode } from './errors/error-codes.js'
 export type { ShapitConfig } from './config/types.js'
 export type { ProjectContext, RuleResult, AuditResult } from './core/types.js'
