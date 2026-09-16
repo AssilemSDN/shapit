cc8dbcdb9fdd73bb43480a4b6747d88c6da33f24
Mélissa <pufferfish.bloop@proton.me>
2026-09-16 14:45:50 +0200

    chore: add shapitconfig type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { loadConfig } from './config/load-config.js'
 export { audit } from './core/audit.js'
 
+export type { ShapitConfig } from './config/types.js'
 export type { ProjectContext, RuleResult, AuditResult } from './core/types.js'
